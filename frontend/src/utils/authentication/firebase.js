c386701bb34ea8b26f61dd799c055548a980f614
Jahaan J <jahaanjain@gmail.com>
2022-04-12 21:00:41 -0400

    firebase initialization

---
+++ frontend/src/utils/authentication/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2NYqoZFQYz9Add78UAlLb6nsv5nlVq3I",
+    authDomain: "ideal-events-4ecd1.firebaseapp.com",
+    projectId: "ideal-events-4ecd1",
+    storageBucket: "ideal-events-4ecd1.appspot.com",
+    messagingSenderId: "111986312707",
+    appId: "1:111986312707:web:9c4b39fd9dea47540e7036",
+};
+
+export const app = initializeApp(firebaseConfig);
