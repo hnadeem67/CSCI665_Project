169977e2ce386210300feac619eac26c6cd715c3
hnadeem67 <hnadeem516@gmail.com>
2022-04-24 18:39:37 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Homepage from "./pages/homepage";
 import Event from "./pages/event";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Profile from "./pages/profile";
 
 // pages
 // import LoginOrRegister from "./components/LoginOrRegister/LoginOrRegister";
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/event/:id" element={<Event />} />
+      <Route path="/profile" element={<Profile />} />
       {/* <Route path="about" element={<About />} /> */}
     </Routes>
   );
